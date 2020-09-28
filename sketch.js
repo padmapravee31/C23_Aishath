703713da7b47bacbc3ea8633a047232ec781257f
padmapravee31 <padmapravee31@gmail.com>
2020-09-28 19:13:19 +0530

    C23

--- sketch.js
+++ sketch.js
@@ -4,6 +4,7 @@ const Bodies = Matter.Bodies;
 
 var engine, world;
 var ground, ball;
+var box1,box2;
 
 function setup(){
     var canvas = createCanvas(400,400);
@@ -14,15 +15,11 @@ function setup(){
         isStatic: true
     }
 
-    ground = Bodies.rectangle(200,390,200,20,ground_options);
+    ground = Bodies.rectangle(200,390,400,20,ground_options);
     World.add(world,ground);
 
-    var ball_options ={
-        restitution: 1.0
-    }
-
-    ball = Bodies.circle(200,100,20, ball_options);
-    World.add(world,ball);
+    box1 = new Box(200,100,50,100);
+    box2 = new Box(300,200,50,50);
 
     console.log(ground);
 }
@@ -33,6 +30,6 @@ function draw(){
     rectMode(CENTER);
     rect(ground.position.x,ground.position.y,400,20);
 
-    ellipseMode(RADIUS);
-    ellipse(ball.position.x, ball.position.y, 20, 20);
+    box1.display();
+    box2.display();
 }
